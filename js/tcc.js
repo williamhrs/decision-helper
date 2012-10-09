3b6c6de7e4f45047b28911d38d2da2b03ff441cb
William Rufino <williamhrs@gmail.com>
2012-10-09 14:43:23 -0300

    easesa

--- js/tcc.js
+++ js/tcc.js
@@ -1,92 +1,11 @@
 $(function(){
 
-var map;
-var heatmap; 
-var bairros = Array();
-var dados = new Object();	
-var nomes_bairros = Array();
-var peso_renda = 0;
-
-
-function getHeatMapColor(value, n)
-{
-  var NUM_COLORS = 3;
-  var color = [ [0,255,0], [255,255,0], [255,0,0] ];
-    // a static array of 3 colors:  (   green,  yellow,  red) using {r,g,b} for each
- 
-  var idx1;        // |-- our desired color will be between these two indexes in "color"
-  var idx2;        // |
-  var fractBetween = 0;  // fraction between "idx1" and "idx2" where our value is
- 
-  if(value <= 0)      {  idx1 = idx2 = 0;            }    // accounts for an input <=0
-  else if(value >= 1)  {  idx1 = idx2 = NUM_COLORS-1; }    // accounts for an input >=0
-  else
-  {
-    value = value * (NUM_COLORS-1);        // will multiply value by 2
-    idx1  = Math.floor(value);                  // our desired color will be after this index
-    idx2  = idx1+1;                        // ... and before this index (inclusive)
-    fractBetween = value - parseFloat(idx1);    // distance between the two indexes (0-1)
-  }
- 
-  red   = ((color[idx2][0] - color[idx1][0])*fractBetween + color[idx1][0]).toFixed(0);
-  green = ((color[idx2][1] - color[idx1][1])*fractBetween + color[idx1][1]).toFixed(0);
-  blue  = ((color[idx2][2] - color[idx1][2])*fractBetween + color[idx1][2]).toFixed(0);
-
-  return  'rgb('+red+','+green+', '+blue+')';
-
-}
-	$('#slider-renda').slider({
-		min:1,
-		max:10,
-		step:0.5,
-		change: function(event,ui){
-			var peso_renda = $('#slider-renda').slider('value')
-			var peso_populacao = $('#slider-populacao').slider('value');
-			if(bairros) {
-				tamanho = nomes_bairros.length;
-				for(var i=0;i<tamanho;++i){
-					media = (bairros[nomes_bairros[i]][1].valorRenda * peso_renda + bairros[nomes_bairros[i]][1].valorPopulacao * peso_populacao) / (peso_populacao + peso_renda);
-					if(media){
-						cores = getHeatMapColor(media, 1 );
-						bairros[nomes_bairros[i]][1].fillColor = cores;
-						bairros[nomes_bairros[i]][1].setMap(map);
-					}
-				}
-			}
-		}
-	});
-
-	$('#slider-populacao').slider({
-		min:1,
-		max:10,
-		step:0.5,
-		slide: function(event,ui){
-			var peso_renda = $('#slider-renda').slider('value')
-			var peso_populacao = $('#slider-populacao').slider('value');
-			if(bairros) {
-				tamanho = nomes_bairros.length;
-				for(var i=0;i<tamanho;++i){
-					media = (bairros[nomes_bairros[i]][1].valorRenda * peso_renda + bairros[nomes_bairros[i]][1].valorPopulacao * peso_populacao) / (peso_populacao + peso_renda);
-					if(media){
-						cores = getHeatMapColor(media, 1 );
-						bairros[nomes_bairros[i]][1].fillColor = cores;
-						bairros[nomes_bairros[i]][1].setMap(map);
-					}
-				}
-			}
-		}
-	});
-
-	$('#slider-empresasti').slider({
-		min:1,
-		max:20,
-		step:0.5,
-		slide: function(event,ui){
-			var peso_empresas = $(this).slider('value');
-			heatmap.heatmap.set('radius',peso_empresas)
-			heatmap.setDataSet(testData);
-		}
-	});
+	var map;
+	var heatmap; 
+	var bairros = []; // Array de Bairros
+	var dados = {};	
+	var nomes_bairros = [];
+	var peso_renda = 0;
 
 	//central coordinates of belo horizonte
 	var myLatlng = new google.maps.LatLng(-19.9167,-43.9333);
@@ -104,6 +23,7 @@ function getHeatMapColor(value, n)
 	  scaleControl: true,
 	  disableDoubleClickZoom: false
 	};
+
 	//google maps instance
 	map = new google.maps.Map(document.getElementById("heatmapArea"), myOptions);
 
@@ -115,6 +35,100 @@ function getHeatMapColor(value, n)
 		loadBairros(loadHeatMapData);
 	});
 
+	//Classe Bairro
+	Bairro = function(nome){
+		this.nome = nome;
+		this.nome_reduzido = nome.replace(/\s/g,'_').toLowerCase();
+		this.poligono = null;
+		this.renda = 0; //Float
+		this.populacao = 0; //Float
+	}
+
+	Bairro.prototype.atualizaCor = function(media) {
+			var cores = getHeatMapColor(media);
+			this.poligono.fillColor = cores;
+			this.poligono.setMap(map);
+	}
+
+
+	$('#slider-renda').slider({
+		min:1,
+		max:10,
+		step:0.5,
+		change: function(event,ui){
+			var peso_renda = $('#slider-renda').slider('value')
+			var peso_populacao = $('#slider-populacao').slider('value');
+			if(bairros) {
+				tamanho = nomes_bairros.length;
+				for(var i=0;i<tamanho;++i){
+					media = (bairros[nomes_bairros[i]].renda * peso_renda + bairros[nomes_bairros[i]].populacao * peso_populacao) / (peso_populacao + peso_renda);
+					if(media){
+						bairros[nomes_bairros[i]].atualizaCor(media);
+					}
+				}
+			}
+		}
+	});
+
+	$('#slider-populacao').slider({
+		min:0.5,
+		max:10,
+		step:0.5,
+		change: function(event,ui){
+			var peso_renda = $('#slider-renda').slider('value')
+			var peso_populacao = $('#slider-populacao').slider('value');
+			if(bairros) {
+				tamanho = nomes_bairros.length;
+				for(var i=0;i<tamanho;++i){
+					media = (bairros[nomes_bairros[i]].renda * peso_renda + bairros[nomes_bairros[i]].populacao * peso_populacao) / (peso_populacao + peso_renda);
+					if(media){
+						bairros[nomes_bairros[i]].atualizaCor(media);
+					}
+				}
+			}
+		}
+	});
+
+	$('#slider-empresasti').slider({
+		min:0,
+		max:10,
+		step:0.5,
+		change: function(event,ui){
+			var peso_empresas = $(this).slider('value');
+			heatmap.heatmap.set('radius',peso_empresas)
+			heatmap.setDataSet(testData);
+		}
+	});
+
+	function getHeatMapColor(value){
+		var NUM_COLORS = 4;
+		var color = [ [0,255,255], [0,255,0], [255,255,0], [255,0,0] ];
+		// a static array of 4 colors:  ( cyan,  green,  yellow,  red) using {r,g,b} for each
+
+		var idx1;        // |-- our desired color will be between these two indexes in "color"
+		var idx2;        // |
+		var fractBetween = 0;  // fraction between "idx1" and "idx2" where our value is
+
+		if(value <= 0){  
+			idx1 = idx2 = 0;
+		}    // accounts for an input <=0
+		else if(value >= 1) { 
+			idx1 = idx2 = NUM_COLORS-1; 
+		}    // accounts for an input >=0
+		else {
+			value = value * (NUM_COLORS-1);        // will multiply value by 2
+			idx1  = Math.floor(value);                  // our desired color will be after this index
+			idx2  = idx1+1;                        // ... and before this index (inclusive)
+			fractBetween = value - parseFloat(idx1);    // distance between the two indexes (0-1)
+		}
+
+		red   = ((color[idx2][0] - color[idx1][0])*fractBetween + color[idx1][0]).toFixed(0);
+		green = ((color[idx2][1] - color[idx1][1])*fractBetween + color[idx1][1]).toFixed(0);
+		blue  = ((color[idx2][2] - color[idx1][2])*fractBetween + color[idx1][2]).toFixed(0);
+
+		return  ['rgb(',red,',',green,',',blue,')'].join('');
+
+	}
 
 	function loadBairros(callback){
 		$.ajax({
@@ -126,15 +140,22 @@ function getHeatMapColor(value, n)
 					nome = $(this).find('description').text()
 					li = $(nome).find('li')[0]
 					//pega o nome do bairro
-					nome_bairro = $(li).find('span').eq(1).text()
-					nome_bairro = nome_bairro.replace(/\s/g,'_').toLowerCase();
+					nome_bairro = $(li).find('span').eq(1).text();
+					nome_bairro_slug = nome_bairro.replace(/\s/g,'_').toLowerCase();
+
+					bairros[nome_bairro_slug] = new Bairro(nome_bairro);
+					bairros[nome_bairro_slug].nome_reduzido = nome_bairro_slug;
+
+
 					coordenadas = $(this).find('LinearRing').find('coordinates');
 					coordenadas = $(coordenadas).text().split(" ");
 					tamanho = coordenadas.length;
-					poligono = Array();
-					bairros[nome_bairro] = Array(nome_bairro);
+					
+					//bairros[nome_bairro] = Array(nome_bairro);
 					cores = getHeatMapColor(0,1);
 
+
+					poligono = [];
 					for (i=0;i<=tamanho;i++) {
 						if($.trim(coordenadas[i]).replace(/\s/g,"") != "") {
 							coord = $.trim(coordenadas[i]).split(',');	
@@ -142,17 +163,17 @@ function getHeatMapColor(value, n)
 						}
 					}
 
-					bairros[nome_bairro].push(new google.maps.Polygon({
+					bairros[nome_bairro_slug].poligono = new google.maps.Polygon({
 							paths: poligono,
 							strokeColor: "#FF0000",
 							strokeOpacity: 0.8,
 							strokeWeight: 2,
 							fillColor: '#ffffff',
-							fillOpacity: 0.5,
-					}));
+							fillOpacity: 0.7,
+					});
 
-					bairros[nome_bairro][1].setMap(map);
-					nomes_bairros.push(nome_bairro);
+					bairros[nome_bairro_slug].poligono.setMap(map);
+					nomes_bairros.push(nome_bairro_slug);
 				});
 			}
 		}).done(callback);
@@ -163,21 +184,13 @@ function getHeatMapColor(value, n)
 	        type: "GET",
 			url: "dados/belo_horizonte_renda_normalizada.json",
 			dataType: "json",
-			beforeSend: function(){
-				if(bairros) {
-					tamanho = nomes_bairros.length;
-					for(var i=0;i<tamanho;++i){
-						//bairros[nomes_bairros[i]][1].setMap(null);
-					}
-				}
-			},
 			success: function(json) {
 				tamanho = json.length;
 				for(var i=0;i<tamanho;++i){
 						nome_bairro = json[i][0].bairro.replace(/\s/g,'_').toLowerCase();
 						if(bairros[nome_bairro]) {
-							bairros[nome_bairro][1]['valorRenda'] = json[i][1]['renda'];
-							bairros[nome_bairro][1]['valorPopulacao'] = json[i][1]['populacao'];
+							bairros[nome_bairro].renda = json[i][1]['renda'];
+							bairros[nome_bairro].populacao = json[i][1]['populacao'];
 						}
 
 				}
